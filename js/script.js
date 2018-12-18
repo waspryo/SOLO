f21d62f0cadd67e969dcb740d9433b613faf1171
waspryo <wasp2123321@gmail.com>
2018-12-18 18:34:41 +0900

    Team done

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(window).on('load', function() { // make sure that whole site is loaded
 /*===================================
               preloader
 ===================================*/
-$(function(){
+$(function() {
   $("#team-members").owlCarousel({
     items: 2,
     autoplay: true,
@@ -18,6 +18,26 @@ $(function(){
     autoplayHoverPause: true,
     nav: true,
     dots: false,
-    navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
   });
+});
+
+/*===================================
+              Progress Bars
+===================================*/
+$(function() {
+
+  $("#progress-elements").waypoint(function() {
+
+    $(".progress-bar").each(function() {
+      $(this).animate({
+        width: $(this).attr("aria-valuenow") + "%"
+      }, 1000);
+    });
+
+    this.destroy();
+  }, {
+    offset: 'bottom-in-view'
+  });
+
 });
