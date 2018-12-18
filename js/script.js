ca09a77adf0b59bf67896a5f1536ec4701b6698c
waspryo <wasp2123321@gmail.com>
2018-12-18 13:27:52 +0900

    team section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ $(window).on('load', function() { // make sure that whole site is loaded
   $('#status').fadeOut();
   $('#preloader').delay(350).fadeOut('slow');
 });
+
+/*===================================
+              preloader
+===================================*/
+$(function(){
+  $("#team-members").owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+  });
+});
