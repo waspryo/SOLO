8964f76fea614d20505c28eee5d0bf6442630d68
waspryo <wasp2123321@gmail.com>
2018-12-21 16:07:22 +0900

    stats

--- js/script.js
+++ js/script.js
@@ -99,3 +99,12 @@ $(function() {
     navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
   });
 });
+/*===================================
+              Stats
+===================================*/
+$(function() {
+  $(".counter").counterUp({
+                delay: 10,
+                time: 3000
+  });
+});
